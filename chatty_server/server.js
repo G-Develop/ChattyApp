cf285ae7763b43956705d38532e34390c8806359
G-Develop <mfaisal.rahman51.996@gmail.com>
2018-10-07 17:58:01 +0000

    mecell

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -1,43 +1,43 @@
 // server.js
-
+//================ SERVER STUFF ==============================
 const express = require('express');
 const SocketServer = require('ws').Server;
-
-// Set the port to 3001
-const PORT = 3001;
-
-// Create a new express server
-const app = express()
- // Make the express server serve static assets (html, javascript, css)
- // from the /public folder
-.use(express.static('public'))
+const uuid = require('uuid');
+const PORT = 3001;  // Set the port to 3001
+const app = express()  // Create a new express server
+.use(express.static('public'))  // Make the express server serve static assets (html, javascript, css)  from the /public folder
 .listen(
   PORT, '0.0.0.0', 'localhost',
   () => console.log(`Listening on ${PORT}`)
 );
+const wsServer = new SocketServer({server: app});  // Create the WebSockets server and attach it to express
+const id = uuid()
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-// Create the WebSockets server and attach it to express
-const wsServer = new SocketServer({server: app});
 
 // Stores the current value of the text box on the client
 let currentContents = '';
 
-// Set up a callback that will run when a client connects to the server
-// When a client connects they are assigned a socket, represented by
-// the `client` argument in the callback.
+
+//========================wsSERVER.ON ==============================
+// Set up a callback that will run when a client connects to the server When a client connects they are assigned a socket, represented by the `client` argument in the callback.
 wsServer.on('connection', (client) => {
   console.log('Client connected');
 
   // Send current textbox contents on connection
   client.send(currentContents);
+  
 
   // Handle messages
-  client.on('message', handleMessage);
+  client.on('message', message => ( (handleMessage(message, id))));
+  
 
   // Set up a callback for when a client closes the socket.
   // This usually means they closed their browser tab.
   client.on('close', () => console.log('Client disconnected'));
 });
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 
 
 // Broadcast - Goes through each client and sends message data
@@ -48,15 +48,17 @@ wsServer.broadcast = function(data) {
 };
 
 
-// Handles incoming messages.
-// Stores the current state of the textbox and broadcasts it
-function handleMessage(message) {
-  console.log("the unparesed message : ", message);
+//=============== FUNCTIONS ===========================
+
+
+// Handles incoming messages &  Stores the current state of the textbox and broadcasts it
+function handleMessage(message, id) {
+  //console.log("the unparesed message : ", message);
   let parsedJson = JSON.parse(message);
-  console.log("the paresed Message ", parsedJson);
-  console.log(`User ${parsedJson["currentUser"]} said ${parsedJson["message"]}`)
-  currentContents = message;
-  wsServer.broadcast(message);
+   parsedJson.id = id
+  //console.log("the paresed Message ", parsedJson);
+  //console.log(`User ${parsedJson.userName} said ${parsedJson.content}`)
+  console.log( parsedJson)
 }
 
 
--- src/App.jsx
+++ src/App.jsx
@@ -9,36 +9,17 @@ class App extends Component {
     super(props);
     this.state = {
       currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
-      messages: [
-        {
-          username: "Bob",
-          content: "Has anyone seen my marbles?",
-        },
-        {
-          username: "Anonymous",
-          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
-        }
-      ]
+      messages: []
+       
     };
   }
 
   componentDidMount() {
     const ws = new WebSocket('ws://localhost:3001')
     console.log("connected to server")
-//    window._ws=ws // this alows the browser to access the ws variable. 
+    /*window._ws=ws  this alows the browser to access the ws variable.*/ 
     this.WebSocket=ws;
-
-
-/*  setTimeout(() => {
-    console.log("Simulating incoming message");
-    // Add a new message to the list of messages in the data store
-    const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-    const messages = this.state.messages.concat(newMessage)
-    // Update the state of the app component.
-    // Calling setState will trigger a call to render() in App and all child components.
-    this.setState({messages: messages})
-  }, 3000);*/
-}
+  }
 
 /*  changeUsername = (username) =>  {
     this.setState({currentUser: {name: username}}, () => {console.log(this.state.currentUser)})
@@ -46,8 +27,8 @@ class App extends Component {
 
   appendMessage = (message) => {
     const messageObj = { 
-      message:message,
-      currentUser:this.state.currentUser.name
+      content:message,
+      userName :this.state.currentUser.name
     } 
     this.WebSocket.send(JSON.stringify( messageObj ))
     const oldstate = {...this.state};
@@ -71,7 +52,7 @@ class App extends Component {
            currentUser = {this.state.currentUser.name}
            user =  {this.changeUsername}
            message = {this.appendMessage}
-        />
+      />
       </div>
     )
   }
